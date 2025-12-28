6b6f95e4bdd78bd18ccdb680156723f614bfedac
Emanuel Mairoll <accounts@emanuelmairoll.at>
2025-12-28 14:27:20 +0100

    Simplify metrics

--- pkg/daemon/backend_metrics.go
+++ pkg/daemon/backend_metrics.go
@@ -19,6 +19,8 @@ import (
 	"net/netip"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+
 	"github.com/scionproto/scion/pkg/addr"
 	"github.com/scionproto/scion/pkg/metrics"
 	"github.com/scionproto/scion/pkg/private/ctrl/path_mgmt"
@@ -27,138 +29,163 @@ import (
 	"github.com/scionproto/scion/pkg/snet"
 )
 
+// BackendMetrics contains all metrics for daemon backend operations.
+type BackendMetrics struct {
+	PathsRequests              metrics.Counter
+	PathsLatency               metrics.Histogram
+	ASRequests                 metrics.Counter
+	ASLatency                  metrics.Histogram
+	InterfacesRequests         metrics.Counter
+	InterfacesLatency          metrics.Histogram
+	ServicesRequests           metrics.Counter
+	ServicesLatency            metrics.Histogram
+	InterfaceDownNotifications metrics.Counter
+	InterfaceDownLatency       metrics.Histogram
+}
+
+// NewBackendMetrics creates a new BackendMetrics with the given namespace.
+func NewBackendMetrics(namespace string) BackendMetrics {
+	return BackendMetrics{
+		PathsRequests: metrics.NewPromCounterFrom(prometheus.CounterOpts{
+			Namespace: namespace,
+			Subsystem: "path",
+			Name:      "requests_total",
+			Help:      "The amount of path requests received.",
+		}, []string{prom.LabelResult, prom.LabelDst}),
+		PathsLatency: metrics.NewPromHistogramFrom(prometheus.HistogramOpts{
+			Namespace: namespace,
+			Subsystem: "path",
+			Name:      "request_duration_seconds",
+			Help:      "Time to handle path requests.",
+			Buckets:   prom.DefaultLatencyBuckets,
+		}, []string{prom.LabelResult}),
+		ASRequests: metrics.NewPromCounterFrom(prometheus.CounterOpts{
+			Namespace: namespace,
+			Subsystem: "as_info",
+			Name:      "requests_total",
+			Help:      "The amount of AS requests received.",
+		}, []string{prom.LabelResult}),
+		ASLatency: metrics.NewPromHistogramFrom(prometheus.HistogramOpts{
+			Namespace: namespace,
+			Subsystem: "as_info",
+			Name:      "request_duration_seconds",
+			Help:      "Time to handle AS requests.",
+			Buckets:   prom.DefaultLatencyBuckets,
+		}, []string{prom.LabelResult}),
+		InterfacesRequests: metrics.NewPromCounterFrom(prometheus.CounterOpts{
+			Namespace: namespace,
+			Subsystem: "if_info",
+			Name:      "requests_total",
+			Help:      "The amount of interfaces requests received.",
+		}, []string{prom.LabelResult}),
+		InterfacesLatency: metrics.NewPromHistogramFrom(prometheus.HistogramOpts{
+			Namespace: namespace,
+			Subsystem: "if_info",
+			Name:      "request_duration_seconds",
+			Help:      "Time to handle interfaces requests.",
+			Buckets:   prom.DefaultLatencyBuckets,
+		}, []string{prom.LabelResult}),
+		ServicesRequests: metrics.NewPromCounterFrom(prometheus.CounterOpts{
+			Namespace: namespace,
+			Subsystem: "service_info",
+			Name:      "requests_total",
+			Help:      "The amount of services requests received.",
+		}, []string{prom.LabelResult}),
+		ServicesLatency: metrics.NewPromHistogramFrom(prometheus.HistogramOpts{
+			Namespace: namespace,
+			Subsystem: "service_info",
+			Name:      "request_duration_seconds",
+			Help:      "Time to handle services requests.",
+			Buckets:   prom.DefaultLatencyBuckets,
+		}, []string{prom.LabelResult}),
+		InterfaceDownNotifications: metrics.NewPromCounter(prom.SafeRegister(
+			prometheus.NewCounterVec(prometheus.CounterOpts{
+				Namespace: namespace,
+				Name:      "received_revocations_total",
+				Help:      "The amount of revocations received.",
+			}, []string{prom.LabelResult, prom.LabelSrc})).(*prometheus.CounterVec),
+		),
+		InterfaceDownLatency: metrics.NewPromHistogramFrom(prometheus.HistogramOpts{
+			Namespace: namespace,
+			Subsystem: "revocation",
+			Name:      "notification_duration_seconds",
+			Help:      "Time to handle interface down notifications.",
+			Buckets:   prom.DefaultLatencyBuckets,
+		}, []string{prom.LabelResult}),
+	}
+}
+
+// ConnectorMetricsWrapper wraps a Connector and records metrics.
 type ConnectorMetricsWrapper struct {
 	Connector
-	Metrics *BackendMetrics
+	Metrics BackendMetrics
 }
 
 // Note: No metrics are collected for LocalIA, PortRange and DRKey functions.
 
-func (c *ConnectorMetricsWrapper) Interfaces(ctx context.Context,
-) (map[uint16]netip.AddrPort, error) {
+func (c *ConnectorMetricsWrapper) Interfaces(ctx context.Context) (map[uint16]netip.AddrPort, error) {
 	start := time.Now()
 	interfaces, err := c.Connector.Interfaces(ctx)
-	c.Metrics.InterfacesRequests.inc(
-		reqLabels{Result: errToMetricResult(err)},
-		time.Since(start).Seconds(),
-	)
+	result := errToResult(err)
+	if c.Metrics.InterfacesRequests != nil {
+		c.Metrics.InterfacesRequests.With(prom.LabelResult, result).Add(1)
+	}
+	if c.Metrics.InterfacesLatency != nil {
+		c.Metrics.InterfacesLatency.With(prom.LabelResult, result).Observe(time.Since(start).Seconds())
+	}
 	return interfaces, unwrapMetricsError(err)
 }
 
-func (c *ConnectorMetricsWrapper) Paths(ctx context.Context, dst, src addr.IA,
-	f PathReqFlags,
-) ([]snet.Path, error) {
+func (c *ConnectorMetricsWrapper) Paths(ctx context.Context, dst, src addr.IA, f PathReqFlags) ([]snet.Path, error) {
 	start := time.Now()
 	paths, err := c.Connector.Paths(ctx, dst, src, f)
-	c.Metrics.PathsRequests.inc(
-		pathReqLabels{Result: errToMetricResult(err), Dst: dst.ISD()},
-		time.Since(start).Seconds(),
-	)
+	result := errToResult(err)
+	if c.Metrics.PathsRequests != nil {
+		c.Metrics.PathsRequests.With(prom.LabelResult, result, prom.LabelDst, dst.ISD().String()).Add(1)
+	}
+	if c.Metrics.PathsLatency != nil {
+		c.Metrics.PathsLatency.With(prom.LabelResult, result).Observe(time.Since(start).Seconds())
+	}
 	return paths, unwrapMetricsError(err)
 }
 
 func (c *ConnectorMetricsWrapper) ASInfo(ctx context.Context, ia addr.IA) (ASInfo, error) {
 	start := time.Now()
 	info, err := c.Connector.ASInfo(ctx, ia)
-	c.Metrics.ASRequests.inc(
-		reqLabels{Result: errToMetricResult(err)},
-		time.Since(start).Seconds(),
-	)
+	result := errToResult(err)
+	if c.Metrics.ASRequests != nil {
+		c.Metrics.ASRequests.With(prom.LabelResult, result).Add(1)
+	}
+	if c.Metrics.ASLatency != nil {
+		c.Metrics.ASLatency.With(prom.LabelResult, result).Observe(time.Since(start).Seconds())
+	}
 	return info, unwrapMetricsError(err)
 }
 
-func (c *ConnectorMetricsWrapper) SVCInfo(ctx context.Context,
-	svcTypes []addr.SVC,
-) (map[addr.SVC][]string, error) {
+func (c *ConnectorMetricsWrapper) SVCInfo(ctx context.Context, svcTypes []addr.SVC) (map[addr.SVC][]string, error) {
 	start := time.Now()
 	info, err := c.Connector.SVCInfo(ctx, svcTypes)
-	c.Metrics.ServicesRequests.inc(
-		reqLabels{Result: errToMetricResult(err)},
-		time.Since(start).Seconds(),
-	)
+	result := errToResult(err)
+	if c.Metrics.ServicesRequests != nil {
+		c.Metrics.ServicesRequests.With(prom.LabelResult, result).Add(1)
+	}
+	if c.Metrics.ServicesLatency != nil {
+		c.Metrics.ServicesLatency.With(prom.LabelResult, result).Observe(time.Since(start).Seconds())
+	}
 	return info, unwrapMetricsError(err)
 }
 
-func (c *ConnectorMetricsWrapper) RevNotification(ctx context.Context,
-	revInfo *path_mgmt.RevInfo,
-) error {
+func (c *ConnectorMetricsWrapper) RevNotification(ctx context.Context, revInfo *path_mgmt.RevInfo) error {
 	start := time.Now()
 	err := c.Connector.RevNotification(ctx, revInfo)
-	c.Metrics.InterfaceDownNotifications.inc(
-		ifDownLabels{Result: errToMetricResult(err), Src: "notification"},
-		time.Since(start).Seconds(),
-	)
+	result := errToResult(err)
+	if c.Metrics.InterfaceDownNotifications != nil {
+		c.Metrics.InterfaceDownNotifications.With(prom.LabelResult, result, prom.LabelSrc, "notification").Add(1)
+	}
+	if c.Metrics.InterfaceDownLatency != nil {
+		c.Metrics.InterfaceDownLatency.With(prom.LabelResult, result).Observe(time.Since(start).Seconds())
+	}
 	return unwrapMetricsError(err)
-}
-
-// Labels used for metrics in the Metrics struct, those labels should be used
-// for initialization.
-var (
-	PathsRequestsLabels              = []string{prom.LabelResult, prom.LabelDst}
-	ASRequestsLabels                 = []string{prom.LabelResult}
-	InterfacesRequestsLabels         = []string{prom.LabelResult}
-	ServicesRequestsLabels           = []string{prom.LabelResult}
-	InterfaceDownNotificationsLabels = []string{prom.LabelResult, prom.LabelSrc}
-	LatencyLabels                    = []string{prom.LabelResult}
-)
-
-// Metrics can be used to inject metrics into the SCION daemon server. Each
-// field may be set individually.
-type BackendMetrics struct {
-	PathsRequests              RequestMetrics
-	ASRequests                 RequestMetrics
-	InterfacesRequests         RequestMetrics
-	ServicesRequests           RequestMetrics
-	InterfaceDownNotifications RequestMetrics
-}
-
-// RequestMetrics contains the metrics for a given request.
-type RequestMetrics struct {
-	Requests metrics.Counter
-	Latency  metrics.Histogram
-}
-
-func (m RequestMetrics) inc(expander interface{ Expand() []string }, latency float64) {
-	if m.Requests != nil {
-		m.Requests.With(expander.Expand()...).Add(1)
-	}
-	if m.Latency != nil {
-		m.Latency.With(expander.Expand()[:2]...).Observe(latency)
-	}
-}
-
-type reqLabels struct {
-	Result string
-}
-
-func (l reqLabels) Expand() []string {
-	return []string{
-		prom.LabelResult, l.Result,
-	}
-}
-
-type pathReqLabels struct {
-	Result string
-	Dst    addr.ISD
-}
-
-func (l pathReqLabels) Expand() []string {
-	return []string{
-		prom.LabelResult, l.Result,
-		prom.LabelDst, l.Dst.String(),
-	}
-}
-
-type ifDownLabels struct {
-	Result string
-	Src    string
-}
-
-func (l ifDownLabels) Expand() []string {
-	return []string{
-		prom.LabelResult, l.Result,
-		prom.LabelSrc, l.Src,
-	}
 }
 
 type metricsError struct {
@@ -170,7 +197,7 @@ func (e metricsError) Error() string {
 	return e.err.Error()
 }
 
-func errToMetricResult(err error) string {
+func errToResult(err error) string {
 	if err == nil {
 		return prom.Success
 	}
