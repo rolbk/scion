5a839453742c0a2905075a4bb341f3ea263ee079
Emanuel Mairoll <accounts@emanuelmairoll.at>
2026-01-08 11:24:33 +0100

    Remove redundant comment

--- pkg/daemon/connector.go
+++ pkg/daemon/connector.go
@@ -13,7 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package daemon provides APIs for querying SCION Daemons.
 package daemon
 
 import (
